bc4bf7a200a858132599dfc344d1538c0adb6c1b
Volodymyr Machula <machulav@gmail.com>
2023-07-27 19:40:56 +0200

    Update text in CLI

--- packages/connery/src/index.ts
+++ packages/connery/src/index.ts
@@ -8,12 +8,12 @@ import validate from './validate';
 
 const program = new Command();
 
-program.name('connery').description('CLI tool for connector development');
-program.command('init').description('initialize connector repository').action(init);
-program.command('add-action').description('add new action to the connector').action(addAction);
+program.name('connery').description('CLI tool for Connery connector development');
+program.command('init').description('initialize a new Connery connector').action(init);
+program.command('add-action').description('add a new action into the connector').action(addAction);
 program
   .command('validate')
   .description('validate connector definition in ./index.js and linked files')
   .action(validate);
-program.command('build').description('build connector').action(build);
+program.command('build').description('build connector and its dependencies').action(build);
 program.parse();
