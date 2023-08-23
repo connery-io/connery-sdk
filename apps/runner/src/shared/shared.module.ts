9dda7ec8ef0af5737ea5fa8543f99b1478ba207d
Volodymyr Machula <machulav@gmail.com>
2023-08-23 21:26:22 +0200

    Update

--- apps/runner/src/shared/shared.module.ts
+++ apps/runner/src/shared/shared.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-//import { Connector } from './connector';
 import { LocalConfigService } from './local-config.service';
 import { RequestService } from './request.service';
-//import { Action } from './action';
 import { ConnectorsService } from './connectors.service';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guard';
@@ -20,37 +18,9 @@ import { MakeService } from './make.service';
       provide: APP_GUARD,
       useClass: AuthGuard,
     },
-    //{
-    //  provide: 'CURRENT_CONNECTOR',
-    //  inject: [ConnectorsService, RequestService],
-    //  useFactory: async (connectorsService: ConnectorsService, requestService: RequestService): Promise<Connector> => {
-    //    const connector = await connectorsService.getConnector(
-    //      requestService.repoOwner,
-    //      requestService.repoName,
-    //      requestService.repoBranch,
-    //    );
-    //    return connector;
-    //  },
-    //},
-    //{
-    //  provide: 'CURRENT_ACTION',
-    //  inject: ['CURRENT_CONNECTOR', RequestService],
-    //  useFactory: (connector: Connector, requestService: RequestService): Action => {
-    //    const action = connector.getAction(requestService.actionKey);
-    //    return action;
-    //  },
-    //},
     OpenAiService,
     MakeService,
   ],
-  exports: [
-    ConnectorsService,
-    RequestService,
-    LocalConfigService,
-    //'CURRENT_CONNECTOR',
-    //'CURRENT_ACTION',
-    OpenAiService,
-    MakeService,
-  ],
+  exports: [ConnectorsService, RequestService, LocalConfigService, OpenAiService, MakeService],
 })
 export class SharedModule {}
