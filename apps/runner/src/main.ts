99aa12dbdb16b512d05eb28f06bb134e8be93a52
Volodymyr Machula <machulav@gmail.com>
2023-09-06 18:07:36 +0200

    Update docs and ports for the apps

--- apps/runner/src/main.ts
+++ apps/runner/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+  await app.listen(4201);
 }
 bootstrap();
