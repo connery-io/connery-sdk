05511301d20590925ca12cbf2a482b128883b2e0
Volodymyr Machula <machulav@gmail.com>
2023-09-09 22:22:50 +0000

    Update API

--- apps/runner/src/clients-api/tools.controller.ts
+++ apps/runner/src/clients-api/tools.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
-import { ErrorResponse } from './types';
+import { ErrorResponse, ObjectResponse } from './types';
 
 @Controller()
 export class ToolsController {
   @Get('/verify-access')
-  verifyAccess(): void | ErrorResponse {
+  verifyAccess(): ObjectResponse<undefined> | ErrorResponse {
     // By default every API endpoint is protected by the AuthGuard. Including this one.
     // And the AuthGuard will throw an exception if the request is not authenticated.
     // This endpoint does not contain any business logic, it is only used to verify if the request is authenticated by the clients.
+
+    return {
+      status: 'success',
+      data: undefined,
+    };
   }
 }
--- apps/runner/src/health/health.controller.ts
+++ apps/runner/src/health/health.controller.ts
@@ -2,6 +2,10 @@ import { Public } from ':src/shared/auth.guard';
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
 
+type HealthResponse = {
+  status: 'ok' | 'error';
+};
+
 @Controller('/health')
 export class HealthController {
   constructor(private health: HealthCheckService) {}
@@ -9,7 +13,17 @@ export class HealthController {
   @Public()
   @Get()
   @HealthCheck()
-  check() {
-    return this.health.check([]);
+  async check(): Promise<HealthResponse> {
+    try {
+      await this.health.check([]);
+    } catch (error) {
+      return {
+        status: 'error',
+      };
+    }
+
+    return {
+      status: 'ok',
+    };
   }
 }
