4e710c0031511e77642428011f8921bb2ce55070
Volodymyr Machula <machulav@gmail.com>
2023-09-12 08:52:14 +0000

    Update

--- .devcontainer/create-dot-env.sh
+++ .devcontainer/create-dot-env.sh
@@ -6,9 +6,9 @@ cp .env.example .env
 # Add a comment to the top of the file
 sed -i '1i\
 # This file was created by ./.devcontainer/create-dot-env.sh script.\
-# The script also generated a random CONNERY_RUNNER_API_KEY which you can use to authenticate with the runner or you can set your own.\
-# This file is ignored by git so you can use secrets here.\
-# After the devcontainer is rebuilt, the .env file will be reset and the API key will change.\
+# The script also generated a random CONNERY_RUNNER_API_KEY, which you can use to authenticate with the runner, or you can set your own.\
+# This file is ignored by git, so that you can use secrets here.\
+# After the dev container is rebuilt, the .env file will be reset, and the API key will change.\
 ' .env
 
 # Set CONNERY_RUNNER_API_KEY to a randomly generated value
