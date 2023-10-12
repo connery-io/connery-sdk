edec9451911f883155dfc0c95504d65a9c83e771
Volodymyr Machula <machulav@gmail.com>
2023-10-12 17:24:06 +0200

    Add /actions/identify API endpoint (#26)
    * Add /actions/identify API endpoint
    
    * Fix issue

--- apps/runner/src/clients-api/actions.controller.ts
+++ apps/runner/src/clients-api/actions.controller.ts
@@ -1,6 +1,8 @@
 import { OpenAiService } from ':src/shared/openai.service';
 import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import {
+  ActionIdentifiedOutput,
+  ActionNotIdentifiedOutput,
   ErrorResponse,
   ObjectResponse,
   RunActionWithPromptInput,
@@ -12,12 +14,38 @@ import {
 export class ActionsController {
   constructor(private openAiService: OpenAiService) {}
 
+  // TODO: Deprecate this endpoint
   @Post('/run')
   async runAction(
     @Body() body: RunActionWithPromptInput,
   ): Promise<ObjectResponse<RunActionWithPromptOutput1> | ObjectResponse<RunActionWithPromptOutput2> | ErrorResponse> {
     try {
       const result = await this.openAiService.runAction(body.prompt);
+
+      return {
+        status: 'success',
+        data: result,
+      };
+    } catch (error: any) {
+      // TODO: Replace with proper solution
+      throw new HttpException(
+        {
+          status: 'error',
+          error: {
+            message: error.message,
+          },
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Post('/identify')
+  async identifyAction(
+    @Body() body: RunActionWithPromptInput,
+  ): Promise<ObjectResponse<ActionIdentifiedOutput | ActionNotIdentifiedOutput> | ErrorResponse> {
+    try {
+      const result = await this.openAiService.identifyAction(body.prompt);
 
       return {
         status: 'success',
