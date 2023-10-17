b943bf4c1694744a5b480ac93bcd3dfc410a42eb
Volodymyr Machula <machulav@gmail.com>
2023-10-17 14:46:00 +0000

    Remove "Run action with prompt" API endpoint and all the related resources

--- apps/runner/src/clients-api/actions.controller.ts
+++ apps/runner/src/clients-api/actions.controller.ts
@@ -5,44 +5,16 @@ import {
   ActionNotIdentifiedOutput,
   ErrorResponse,
   ObjectResponse,
-  RunActionWithPromptInput,
-  RunActionWithPromptOutput1,
-  RunActionWithPromptOutput2,
+  IdentifyActionInput,
 } from './types';
 
 @Controller('/actions')
 export class ActionsController {
   constructor(private openAiService: OpenAiService) {}
 
-  // TODO: Deprecate this endpoint
-  @Post('/run')
-  async runAction(
-    @Body() body: RunActionWithPromptInput,
-  ): Promise<ObjectResponse<RunActionWithPromptOutput1> | ObjectResponse<RunActionWithPromptOutput2> | ErrorResponse> {
-    try {
-      const result = await this.openAiService.runAction(body.prompt);
-
-      return {
-        status: 'success',
-        data: result,
-      };
-    } catch (error: any) {
-      // TODO: Replace with proper solution
-      throw new HttpException(
-        {
-          status: 'error',
-          error: {
-            message: error.message,
-          },
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-
   @Post('/identify')
   async identifyAction(
-    @Body() body: RunActionWithPromptInput,
+    @Body() body: IdentifyActionInput,
   ): Promise<ObjectResponse<ActionIdentifiedOutput | ActionNotIdentifiedOutput> | ErrorResponse> {
     try {
       const result = await this.openAiService.identifyAction(body.prompt);
--- apps/runner/src/shared/openai.service.ts
+++ apps/runner/src/shared/openai.service.ts
@@ -1,32 +1,12 @@
 import { Inject } from '@nestjs/common';
 import { ActionSchemaType } from 'lib';
 import { ConnectorsService } from ':src/shared/connectors.service';
-import { Configuration, OpenAIApi } from 'openai';
 import { LocalConfigService } from ':src/shared/local-config.service';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { HumanMessage } from 'langchain/schema';
 
 // TODO: replace with the shared type(s),
 // the same that is used in the API response
-// TODO: Deprecate this method
-export type RunActionOutput = {
-  response: string;
-  output?: {
-    [key: string]: string;
-  };
-  used: {
-    prompt: string;
-    connectorKey?: string;
-    actionKey?: string;
-    inputParameters?: {
-      [key: string]: string;
-    };
-  };
-};
-
-// TODO: replace with the shared type(s),
-// the same that is used in the API response
-
 // TODO: add missing required input parameters, and all the other parameters that are not required
 export type ActionIdentifiedOutput = {
   identified: true;
@@ -54,108 +34,6 @@ export class OpenAiService {
     @Inject(ConnectorsService) private connectorsService: ConnectorsService,
     private configService: LocalConfigService,
   ) {}
-
-  // TODO: Deprecate this method
-  async runAction(prompt: string): Promise<RunActionOutput> {
-    if (!prompt) {
-      throw new Error("Input parameter 'prompt' is required but the value is empty or not provided");
-    }
-
-    console.log(JSON.stringify({ type: 'user_prompt_received', data: { prompt: prompt } }));
-
-    const runnerConfig = this.configService.getRunnerConfig();
-
-    if (!runnerConfig.OpenAiApiKey) {
-      throw new Error('The OPENAI_API_KEY is not configured on the runner.');
-    }
-
-    const configuration = new Configuration({
-      apiKey: runnerConfig.OpenAiApiKey,
-    });
-
-    const exposedActions = await this.getExposedActionsJsonSchema();
-
-    // Ask OpenAI to classify the user prompt based on the exposed actions
-    const openai = new OpenAIApi(configuration);
-    const completion1 = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo-0613',
-      messages: [
-        { role: 'system', content: this.getGenearlSystemInstructions() },
-        { role: 'user', content: prompt },
-      ],
-      functions: exposedActions,
-    });
-    const result1 = completion1.data.choices[0];
-
-    console.log(JSON.stringify({ type: 'openai_response_1', data: result1 }));
-
-    if (
-      result1.finish_reason === 'function_call' &&
-      result1.message?.function_call?.name &&
-      result1.message?.function_call?.arguments
-    ) {
-      // If OpenAI classified the user prompt as a function call, run the action
-
-      const actionKey = result1.message.function_call.name;
-      const actionArguments = JSON.parse(result1.message.function_call.arguments);
-
-      // Run the action
-      const action = await this.connectorsService.getAction(actionKey);
-
-      let actionResult;
-      try {
-        actionResult = await action.runAction(actionArguments);
-      } catch (error: any) {
-        throw new Error(error.message);
-      }
-
-      console.log(JSON.stringify({ type: 'action_result', data: actionResult }));
-
-      // Ask OpenAI to generate response for the user based on the action result
-      const completion2 = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo-0613',
-        messages: [
-          {
-            role: 'system',
-            content: this.getGenearlSystemInstructions(),
-          },
-          { role: 'user', content: prompt },
-          {
-            role: result1.message.role,
-            content: result1.message.content,
-            function_call: {
-              name: result1.message.function_call.name,
-              arguments: result1.message.function_call.arguments,
-            },
-          },
-          { role: 'function', name: actionKey, content: this.getResultInstructions(action.schema, actionResult) },
-        ],
-      });
-
-      const result2 = completion2.data.choices[0];
-      console.log(JSON.stringify({ type: 'openai_response_2', data: result2 }));
-
-      return {
-        response: result2.message?.content ?? '',
-        output: actionResult.output,
-        used: {
-          prompt: prompt,
-          connectorKey: actionResult.usedConnectorKey,
-          actionKey: actionResult.usedActionKey,
-          inputParameters: actionResult.usedInputParameters,
-        },
-      };
-    } else {
-      // If OpenAI classified the user prompt as a regular message, return it to the user
-
-      return {
-        response: result1.message?.content ?? '',
-        used: {
-          prompt: prompt,
-        },
-      };
-    }
-  }
 
   async identifyAction(prompt: string): Promise<ActionIdentifiedOutput | ActionNotIdentifiedOutput> {
     // TODO implement
@@ -211,50 +89,6 @@ export class OpenAiService {
         prompt,
       },
     };
-  }
-
-  // TODO: Deprecate this method
-  private getResultInstructions(actionSchema: ActionSchemaType, actionResult: object): string {
-    const parametersInfo: string[] = [];
-
-    for (const outputParameter of actionSchema.outputParameters) {
-      parametersInfo.push(`
-        - Key: ${outputParameter.key}
-          Title: ${outputParameter.title}
-          Description: ${outputParameter.description}
-      `);
-    }
-
-    return `
-      [Instructions for the assistant, not for the user]
-
-      Instructions about the result:
-      - Result is always a stringified JSON that should be unwrapped and converted to the readeable format for the user.
-      - In case of error, show the exact error message text to the user.
-      - Always respond to the user using the language that the user used in the prompt.
-      
-      Action description for the additional context:
-      - Key: ${actionSchema.key}
-        Title: ${actionSchema.title}
-        Description: ${actionSchema.description}
-
-      Output parameters description for the additional context: 
-      ${parametersInfo.join('\n')}
-
-      [Result for the user]
-      ${JSON.stringify(actionResult)}
-    `;
-  }
-
-  // TODO: Deprecate this method
-  private getGenearlSystemInstructions(): string {
-    return `
-      You are a helpful assistant.
-      Your task is to help the user to run functions from Connery.
-      Connery is a connector platform. Connector is a set of actions (functions) that can be run by the user. 
-      Always "actions" term instead of "functions" when talking to the user.
-      If the user asks to run an action, but not all the required input parameters are provided, ask the user to provide the missing input parameters, and never set the values of the input parameters by yourself.
-    `;
   }
 
   private async getExposedActionsJsonSchema() {
