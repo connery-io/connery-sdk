e122ade594732b181a998fc34869c3368c60b990
Volodymyr Machula <machulav@gmail.com>
2023-10-27 00:52:58 +0200

    Remove build command

--- packages/connery/src/index.ts
+++ packages/connery/src/index.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import build from './build';
 import init from './init';
 import addAction from './add-action';
 import validate from './validate';
@@ -26,5 +25,4 @@ program
   .command('validate')
   .description('validate connector definition in ./index.js and linked files')
   .action(validate);
-program.command('build').description('build connector and its dependencies').action(build);
 program.parse();
--- packages/connery/src/validate.ts
+++ packages/connery/src/validate.ts
@@ -6,10 +6,22 @@ export default async function (): Promise<void> {
     logEmptyLine();
     logInfo('🔎 Validating plugin definition in ./dist/plugin.js file...');
 
-    // TODO: check if plugin.js exists and ask to build if not
+    const pluginDefinitionPath = `${process.cwd()}/dist/plugin.js`;
 
+    // Check if ./dist/plugin.js exists
+    try {
+      await import(pluginDefinitionPath);
+    } catch (error: any) {
+      if (error.code === 'ERR_MODULE_NOT_FOUND') {
+        throw new Error('Plugin definition file ./dist/plugin.js is not found. Please build the plugin first.');
+      } else {
+        throw error;
+      }
+    }
+
+    // Check if ./dist/plugin.js exports a valid plugin definition
     // _pluginKey and _configurationParametersObject are not required for validation
-    const pluginLoader = new PluginLoader(`${process.cwd()}/dist/plugin.js`, '', {});
+    const pluginLoader = new PluginLoader(pluginDefinitionPath, '', {});
     await pluginLoader.load();
 
     logSuccess('Plugin definition is valid');
