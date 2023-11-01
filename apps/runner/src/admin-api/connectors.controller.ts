9449d7afa11026ef0adedf34abc2581a5f9437a8
Volodymyr Machula <machulav@gmail.com>
2023-11-01 21:54:21 +0100

    Update

--- apps/runner/src/admin-api/connectors.controller.ts
+++ apps/runner/src/admin-api/connectors.controller.ts
@@ -1,4 +1,5 @@
 import { IPluginCache } from ':src/shared/plugin-cache/plugin-cache.interface';
+import { ObjectResponse } from ':src/shared/types';
 import { Controller, Get, Inject } from '@nestjs/common';
 
 @Controller('/v1/admin/plugins')
@@ -6,8 +7,13 @@ export class PluginsController {
   constructor(@Inject(IPluginCache) private pluginCache: IPluginCache) {}
 
   @Get('/refresh')
-  async refreshPluginCache(): Promise<void> {
+  async refreshPluginCache(): Promise<ObjectResponse<undefined>> {
     await this.pluginCache.clear();
     await this.pluginCache.initialize();
+
+    return {
+      status: 'success',
+      data: undefined,
+    };
   }
 }
