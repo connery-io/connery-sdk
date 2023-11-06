c647f6d066f58039e9b78452e139d93886f3581e
Volodymyr Machula <machulav@gmail.com>
2023-11-07 00:53:25 +0100

    Remove custom dev container

--- .devcontainer/post-start-commands.sh
+++ .devcontainer/post-start-commands.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Restore project cache from the image
-cp -r /connery-cache/project/* /workspaces/connery/
-
 # Create .env file
 ./.devcontainer/create-dot-env.sh
-
-# Use yarn cache from the image
-yarn config set cache-folder /connery-cache/yarn/
 
 # Install dependencies
 yarn install --prefer-offline
