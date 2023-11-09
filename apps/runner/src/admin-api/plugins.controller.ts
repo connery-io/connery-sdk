524b5a2b6d5a9b429474a14ed9bec3d724e33c14
Volodymyr Machula <machulav@gmail.com>
2023-11-09 10:47:18 +0100

    Deprecate old API endpoints and add new ones (#46)

--- apps/runner/src/admin-api/plugins.controller.ts
+++ apps/runner/src/admin-api/plugins.controller.ts
@@ -2,12 +2,23 @@ import { IPluginCache } from ':src/shared/plugin-cache/plugin-cache.interface';
 import { ObjectResponse } from ':src/shared/types';
 import { Controller, Get, Inject } from '@nestjs/common';
 
-@Controller('/v1/admin/plugins')
+@Controller()
 export class PluginsController {
   constructor(@Inject(IPluginCache) private pluginCache: IPluginCache) {}
 
-  @Get('/refresh')
-  async refreshPluginCache(): Promise<ObjectResponse<undefined>> {
+  // This endpoint is deprecated and will be removed in the future
+  // TODO: Remove this endpoint once all the clients are updated to use the new one
+  @Get('/admin/connectors/refresh')
+  async refreshPluginCacheV0(): Promise<ObjectResponse<undefined>> {
+    return this.refreshPluginCache();
+  }
+
+  @Get('/v1/admin/plugins/refresh')
+  async refreshPluginCacheV1(): Promise<ObjectResponse<undefined>> {
+    return this.refreshPluginCache();
+  }
+
+  private async refreshPluginCache(): Promise<ObjectResponse<undefined>> {
     await this.pluginCache.clear();
     await this.pluginCache.initialize();
 
--- apps/runner/src/clients-api/actions.controller.ts
+++ apps/runner/src/clients-api/actions.controller.ts
@@ -1,15 +1,34 @@
-import { Body, Controller, HttpException, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ILlm } from ':src/shared/llm/llm.interface';
 import { ActionIdentifiedOutput, ActionNotIdentifiedOutput } from ':src/shared/llm/types';
 import { ObjectResponse } from ':src/shared/types';
 
-@Controller('/v1/actions')
+type IdentifyActionBody = {
+  prompt: string;
+};
+
+@Controller()
 export class ActionsController {
   constructor(@Inject(ILlm) private llm: ILlm) {}
 
-  @Post('/identify')
-  async identifyAction(
-    @Body() body: { prompt: string },
+  // This endpoint is deprecated and will be removed in the future
+  // TODO: Remove this endpoint once all the clients are updated to use the new one
+  @Post('/actions/identify')
+  async identifyActionV0(
+    @Body() body: IdentifyActionBody,
+  ): Promise<ObjectResponse<ActionIdentifiedOutput | ActionNotIdentifiedOutput>> {
+    return this.identifyAction(body);
+  }
+
+  @Post('/v1/actions/identify')
+  async identifyActionV1(
+    @Body() body: IdentifyActionBody,
+  ): Promise<ObjectResponse<ActionIdentifiedOutput | ActionNotIdentifiedOutput>> {
+    return this.identifyAction(body);
+  }
+
+  private async identifyAction(
+    body: IdentifyActionBody,
   ): Promise<ObjectResponse<ActionIdentifiedOutput | ActionNotIdentifiedOutput>> {
     const result = await this.llm.identifyAction(body.prompt);
 
