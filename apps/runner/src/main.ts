b36a8609f259d6d09aa0351068715da8b9aa55d5
Volodymyr Machula <machulav@gmail.com>
2023-11-25 14:07:08 +0100

    Update

--- apps/runner/src/main.ts
+++ apps/runner/src/main.ts
@@ -4,6 +4,7 @@ import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const runnerPort = 4201;
+  // CORS is required for the OpenAPI specification.
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(runnerPort);
