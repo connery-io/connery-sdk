3c5cf56d32d205c7add5ee054cdced606ee708f9
Volodymyr Machula <machulav@gmail.com>
2023-11-27 23:53:01 +0100

    Fix CLI

--- packages/lib/src/id-utils.ts
+++ packages/lib/src/id-utils.ts
@@ -1,15 +1,13 @@
-import * as crypto from 'crypto';
+import { SHA256 } from 'crypto-js';
 
 export function generatePluginHashId(pluginKey: string): string {
-  const hash = crypto.createHash('sha256');
-  hash.update(pluginKey);
+  const hash = SHA256(pluginKey).toString();
   // CP means "Connery Plugin"
-  return 'CP' + hash.digest('hex').substring(0, 30).toUpperCase();
+  return 'CP' + hash.substring(0, 30).toUpperCase();
 }
 
 export function generateActionHashId(pluginKey: string, actionKey: string): string {
-  const hash = crypto.createHash('sha256');
-  hash.update(`${pluginKey}:${actionKey}`);
+  const hash = SHA256(`${pluginKey}:${actionKey}`).toString();
   // CA means "Connery Action"
-  return 'CA' + hash.digest('hex').substring(0, 30).toUpperCase();
+  return 'CA' + hash.substring(0, 30).toUpperCase();
 }
