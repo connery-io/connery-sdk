746e3848339a46af6739642e75eacb632de57626
Volodymyr Machula <machulav@gmail.com>
2024-01-26 00:06:25 +0100

    Clean up

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Connery",
+  description: "",
 };
 
 export default function RootLayout({
